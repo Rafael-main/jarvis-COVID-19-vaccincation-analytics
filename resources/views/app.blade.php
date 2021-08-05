ae0333a9855ebe863b6a4c6e7d8553799a8d13f8
mjOmandam07 <mjomandam@gmail.com>
2021-08-05 23:50:40 +0800

    reverted the css to use the app.css file in the resources folder

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="/css/index.css">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body class="antialiased">
         <div id="app"></div>
